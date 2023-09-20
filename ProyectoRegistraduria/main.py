ef45c80de5b6de29adcf3f35edff7a2527746267
jquiñonez2023 <jpquinonez02@gmail.com>
2023-09-20 15:21:09 -0500

    Se implementa la tabla Mesa con el CRUD

--- ProyectoRegistraduria/main.py
+++ ProyectoRegistraduria/main.py
@@ -5,37 +5,47 @@ from flask_cors import CORS
 import json
 from waitress import serve
 
-#from Controladores.ControladorEstudiante import ControladorEstudiante
+
+from Controladores.ControladorMesa import ControladorMesa
 
 app = Flask(__name__)
 cors = CORS(app)
 
-
-"**************** Implementacion de los controladores ***********"
-
-#miControladorEstudiante = ControladorEstudiante()
+miControladorMesa = ControladorMesa()
 
 
+@app.route("/mesa", methods=['GET'])
+def getMesas():
+    json = miControladorMesa.index()
+    return jsonify(json)
 
-" ***************** Fin controladores **************************** "
+@app.route("/mesa", methods=['POST'])
+def crearMesa():
+    data = request.get_json()
+    json = miControladorMesa.create(data)
+    return jsonify(json)
 
+@app.route("/mesa/<string:id>", methods=['GET'])
+def getmesa(id):
+    json = miControladorMesa.show(id)
+    return jsonify(json)
 
-"-----------------------------------"
-"--Implementacion de los metodos--"
-"-----------------------------------"
+@app.route("/mesa/<string:id>", methods=['PUT'])
+def modificarMesa(id):
+    data = request.get_json()
+    json = miControladorMesa.update(id, data)
+    return jsonify(json)
 
-"       --------Servicios Canditado CRUD----------      "
+@app.route("/mesa/<string:id>", methods=['DELETE'])
+def eliminarMesa(id):
+    json = miControladorMesa.delete(id)
+    return jsonify(json)
 
+@app.route("/mesa/<string:id>", methods=['GET'])
+def getMesa(id):
+    json = miControladorMesa.show(id)
+    return jsonify(json)
 
-"       --------Servicios Partido CRUD------------      "
-
-
-"       --------Servicios Mesa CRUD---------------      "
-
-
-"       --------Servicios Resultado CRUD----------      "
-
-"--------------------------------------------------------------"
 
 
 # Servicio que el servidor ofrecerá, y este consiste en retornar un JSON el cual
